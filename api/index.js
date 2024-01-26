7e2e45c8f16e35ca9ba0da015b826d1057b257bb
Zaveer Khan <zaveerkhan.fire@gmail.com>
2024-01-27 00:18:31 +0500

    created a test api route

--- api/index.js
+++ api/index.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoute from './routes/userRoute.js';
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
     console.log('MongoDB is connected');
-  }).catch((error) => {
+  })
+  .catch((error) => {
     console.log(error);
   });
 
@@ -15,3 +19,5 @@ const app = express();
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
+app.use('/api/user', userRoute);
