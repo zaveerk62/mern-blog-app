fd74d398010b3f00188d9da7812f6c0584c512f3
Zaveer Khan <zaveerkhan.fire@gmail.com>
2024-01-27 01:03:22 +0500

    created signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoute from './routes/userRoute.js';
+import authRoute from './routes/authRoute.js';
 
 dotenv.config();
 
@@ -15,9 +16,11 @@ mongoose
   });
 
 const app = express();
+app.use(express.json())
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
 
 app.use('/api/user', userRoute);
+app.use('/api/auth', authRoute);
